b9cce00227c263f3fec298f70b96c3a428f79f96
Deepa <gdeepareddy85@gmail.com>
2017-06-22 11:58:06 +0530

    initial commit

--- src/main/java/com/training/controller/AttendanceReportController.java
+++ src/main/java/com/training/controller/AttendanceReportController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.training.DTO.StudentAttendanceDTO;
 import com.training.service.AttendanceReportService;
 
+
 @CrossOrigin
 @RestController
 @RequestMapping("/attendanceReport")
